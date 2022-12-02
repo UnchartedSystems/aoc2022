ab7f437a9a68dc4d01c4f7db6e7699ae4fe9bfb4
Uncharted Systems <dan@uncharted.systems>
2022-12-01 23:42:01 -0700

    day 1

---
+++ day1/sol.clj
@@ -0,0 +1,14 @@
+(:require '[clojure.string])
+
+(last (sort (map #(apply + (map read-string %)) (take-nth 2 (partition-by #(= "" %) (clojure.string/split-lines (slurp "input.txt")))))))
+
+(apply + (take-last 3 (sort (map #(apply + (map read-string %)) (take-nth 2 (partition-by #(= "" %) (clojure.string/split-lines (slurp "input.txt"))))))))
+
+
+;; More readable
+(let [split-strings   (clojure.string/split-lines (slurp "input.txt"))
+      inventories     (take-nth 2 (partition-by #(= "" %) split-strings))
+      sorted-calories (sort (map #(apply + (map read-string %)) inventories))]
+
+  (last sorted-calories)                   ; Part 1
+  (apply + (take-last 3 sorted-calories))) ; Part 2
