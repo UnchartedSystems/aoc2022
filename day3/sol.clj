c45259718ab4b22af346f53a154dc117b9b42b9e
Uncharted Systems <dan@uncharted.systems>
2022-12-04 00:24:19 -0700

    hacked together with no readability

--- day3/sol.clj
+++ day3/sol.clj
@@ -7,6 +7,10 @@
 (defn uppercase-offset [s]
   (if  (= s (str/upper-case s)) 38 96))
 
+
 (def inventories (mapv (fn [sack] (mapv #(into #{} %) (partition (/ (count sack) 2) (str/split sack #"")))) (str/split-lines (slurp "input.txt"))))
 (def results (mapv #(some identity %) (mapv (fn [invs] (map #(if (contains? (second invs) % ) % false) (first invs))) inventories)))
 (apply + (mapv (fn [s] (- (int (first s)) (uppercase-offset s))) results))
+
+(def rucksacks (mapv #(into #{} (str/split % #"")) (str/split-lines (slurp "input.txt"))))
+(apply + (mapv (fn [s] (- (int (first s)) (uppercase-offset s))) (mapv #(some identity %) (mapv (fn [group] (mapv #(if (= true (contains? (second group) %) (contains? (second (rest group)) %)) % false ) (first group))) (partition 3 rucksacks)))))
