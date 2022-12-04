448f3b18b5c5942023f95f56c2d005b155d2f37e
Uncharted Systems <dan@uncharted.systems>
2022-12-04 05:11:26 -0700

    revised to be more concise and readable

--- day3/sol.clj
+++ day3/sol.clj
@@ -1,16 +1,20 @@
 (require '[clojure.string :as str])
-
-
-(map int (seq (char-array "abcdefghijklmnopqrstuvwxyz")))
-(map int (seq (char-array (str/upper-case "abcdefghijklmnopqrstuvwxyz"))))
+(require '[clojure.set :as set])
 
 (defn uppercase-offset [s]
   (if  (= s (str/upper-case s)) 38 96))
 
+(let [rucksacks (str/split-lines (slurp "input.txt"))
+      ;; Part 1
+      get-inventory  #(mapv set (partition (/ (count %) 2) (str/split % #"")))
+      inventories     (mapv get-inventory rucksacks)
+      get-copy        (fn [sets] (first (apply set/intersection sets)))
+      copies          (mapv get-copy inventories)
+      str->priority   (fn [s] (- (int (first s)) (uppercase-offset s)))
+      result-1        (apply + (mapv str->priority copies))
+      ;; Part 2
+      rucksets        (partition 3 (mapv #(into #{} (str/split % #"")) rucksacks))
+      badges          (mapv get-copy rucksets)
+      result-2        (apply + (mapv str->priority badges))]
 
-(def inventories (mapv (fn [sack] (mapv #(into #{} %) (partition (/ (count sack) 2) (str/split sack #"")))) (str/split-lines (slurp "input.txt"))))
-(def results (mapv #(some identity %) (mapv (fn [invs] (map #(if (contains? (second invs) % ) % false) (first invs))) inventories)))
-(apply + (mapv (fn [s] (- (int (first s)) (uppercase-offset s))) results))
-
-(def rucksacks (mapv #(into #{} (str/split % #"")) (str/split-lines (slurp "input.txt"))))
-(apply + (mapv (fn [s] (- (int (first s)) (uppercase-offset s))) (mapv #(some identity %) (mapv (fn [group] (mapv #(if (= true (contains? (second group) %) (contains? (second (rest group)) %)) % false ) (first group))) (partition 3 rucksacks)))))
+  [result-1 result-2])
