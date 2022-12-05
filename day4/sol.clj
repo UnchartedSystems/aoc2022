4321ddd0d55c1ba71968fb71e15d2969d2899857
Uncharted Systems <dan@uncharted.systems>
2022-12-05 04:43:51 -0700

    finished day 4

--- day4/sol.clj
+++ day4/sol.clj
@@ -1,5 +1,16 @@
 (require '[clojure.string :as str])
 
+;; destructuring and transforming input into pairs of ranges
 (def lines (str/split-lines (slurp "input.txt")))
-(def pairs (mapv (fn [line] (partition 2 (mapv read-string (str/split line #"(,|-)")))) lines))
-(def ranges (mapv (fn [ranges] ()) pairs))
+(def ranges (mapv #(partition 2 (mapv read-string (str/split % #"(,|-)"))) lines))
+
+;; functions that check how ranges intersect
+(def contains-range? #(or (and (<= %1 %2) (>= %3 %4))
+                          (and (<= %2 %1) (>= %4 %3))))
+
+(def overlaps-range? #(= false (< %3 %2) (< %4 %1)))
+
+(def range-tests [contains-range? overlaps-range?])
+
+;; returns solutions to 1 & 2 in a pair by mapping over range-tests
+(mapv #(apply + (mapv (fn [[[x-1 x-2] [y-1 y-2]]] (if (% x-1 y-1 x-2 y-2) 1 0)) ranges)) range-tests)
